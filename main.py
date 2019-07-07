b6c5ee777ef284257dc9aaf39fa2541c37544a15
Hernan <hernantion1@yahoo.com>
2019-07-07 18:36:31 -0500

    detecting optical disc by the quantity of pixel brightest

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import RRO_thresholding
+import copy
+from sklearn.cluster import KMeans
 
-filename = "images/im0011.ppm"
-#filename = "../dataset_retinas/DRIVE/19_test.tif"
+filename = "images/im0002.ppm"
+#filename = "../dataset_retinas/DRIVE/28_training.tif"
 #problemas
 #filename = "images/im0014.ppm"
 #filename = "images/im0023.ppm"
@@ -24,6 +26,8 @@ def histogram(image):
     plt.plot(hist)
     plt.xlim([0, 256])
     plt.show()
+
+
 
 def removing_dark_pixel(image):
 
@@ -57,9 +61,7 @@ def threshold(img,t):
     #creating binary matrix
     for i in range(0,rows):
     	for j in range(0,cols):
-    		if img[i][j]  >= t:
-    			new_matriz[i][j] = 0
-    		else:
+    		if img[i][j]  == t:
     			new_matriz[i][j] = 1
     return new_matriz
 
@@ -92,39 +94,195 @@ def sobel(image):
 
     return grad
 
-def detecte_circles(new_image,g):
-    cimage = cv2.cvtColor(new_image,cv2.COLOR_GRAY2BGR)
 
-    circles = cv2.HoughCircles(new_image,cv2.HOUGH_GRADIENT,1,20,
+def sort(array,i):
+    """Sort the array by using quicksort."""
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0][i]
+        for x in array:
+            if x[i] < pivot:
+                less.append(x)
+            elif x[i] == pivot:
+                equal.append(x)
+            elif x[i] > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(greater,i)+equal+sort(less,i)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
+
+def count_pixel_veins(veins,x,y):
+    total_veins = 0
+    for i in range(y-50,y+50):
+        for j in (x-50,x+50):
+            if veins[i][j] == 0:
+                total_veins += 1
+
+    return total_veins
+
+def count_pixel_bright(img,x,y):
+    total_veins = 0
+    for i in range(y-10,y+10):
+        for j in (x-10,x+10):
+            total_veins += img[i][j]
+
+    return total_veins
+
+def detect_circles(edge_image,veins,g):
+    cimage = cv2.cvtColor(edge_image,cv2.COLOR_GRAY2BGR)
+
+    circles = cv2.HoughCircles(edge_image,cv2.HOUGH_GRADIENT,1,20,
                             param1=50,param2=30,minRadius=30,maxRadius=90)
     print(circles)
 
     if circles is not  None:
 
         circles = np.uint16(np.around(circles))
-        max = circles[0][0]
+
+
+        intensity = []
+        for i in circles[0,:]:
+                intensity.append([count_pixel_bright(g,i[0],i[1]),count_pixel_veins(veins,i[0],i[1]),i])
+
+        #count_pixel_posibles_veins(veins,i[0],i[1])
+
+        intensity = sort(intensity,0)
+        print("intensity",intensity)
+
+
+        tamanio = 5 #we choose only the 5 first values
+        if len(intensity) < tamanio:
+            tamanio = len(intensity)
+
+        max = intensity[0]
+        for i in range(0,tamanio):
+            if intensity[i][0] > max[0]:
+                max = intensity[i]
+
+        #select only the data of the circle (x y radius)
+        max = max[2]
+        print("maximo",max)
+
+
+
         for i in circles[0,:]:
             # draw the outer circle
             #cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
             # draw the center of the circle
             cv2.circle(cimage,(i[0],i[1]),2,(0,0,255),3)
 
-            #print(image[i[1]][i[0]])
-            print("xxx",max[1],i[1])
-            if g[i[1]][i[0]]  > g[max[1]][max[0]]:
-                max = i
+        for i in range(0,5):
 
-        cv2.circle(cimage,(max[0],max[1]),i[2],(0,250,0),2)
-        cv2.circle(cimage,(max[0],max[1]),2,(0,0,200),3)
+            cv2.circle(cimage,(intensity[i][2][0],intensity[i][2][1]),2,(255,0,0),3)
+
+        cv2.circle(cimage,(max[0],max[1]),max[2],(0,250,0),2)
+        cv2.circle(cimage,(max[0],max[1]),2,(255,0,0),3)
 
 
 
         show_image(cimage,'detected circles')
+
     else:
         print("No se encontro disco optico")
 
-def detect_optical_disc(image):
+def detect_veins(image):
+    b,g,r = cv2.split(image)
+    #show_image(g,"verde")
+    #Crear un kernel de '1' de 3x3 elipse
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(1,1))
 
+    #Se aplica la transformacion: Opening
+    transformacion = cv2.morphologyEx(g,cv2.MORPH_OPEN,kernel)
+    #show_image(transformacion,"transformacion")
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(100,100))
+    tophat = cv2.morphologyEx(transformacion, cv2.MORPH_BLACKHAT, kernel)
+    #show_image(tophat,"transformacion2")
+
+
+
+    Z = tophat.reshape((-1,1))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 2
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    print("center",center)
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((tophat.shape))
+    res2 = threshold(res2,center[0])
+    show_image(res2,"kmeans")
+
+    return res2
+
+    """
+    X = tophat.reshape((-1, 1))
+
+    kmeans = KMeans(n_clusters=2)
+    kmeans.fit(X)
+    centroids = kmeans.cluster_centers_.squeeze()
+    labels = kmeans.labels_
+    # Assign each value to the nearest centroid and
+    # reshape it to the original image shape
+    input_image_compressed = np.choose(labels, centroids).reshape(tophat.shape)
+    show_image(input_image_compressed,"threshold")
+    """
+
+def detect_veins2(image):
+    #Kirsch's Templates
+
+    #image = cv2.GaussianBlur(image,(5,5),0)
+    h1 = np.matrix("-5 -3 -3; 5 0 -3; 5 -3 -3")
+    h2 = np.matrix("-3 -3 5; -3 0 5; -3 -3 5")
+    h3 = np.matrix("-3 -3 -3; 5 0 -3; 5 5 -3")
+    h4 = np.matrix("-3 5 5; -3 0 5; -3 -3 -3")
+    h5 = np.matrix("-3 -3 -3; -3 0 -3; 5 5 5")
+    h6 = np.matrix("5 5 5; -3 0 -3; -3 -3 -3")
+    h7 = np.matrix("-3 -3 -3; -3 0 5; -3 5 5")
+    h8 = np.matrix("5 5 -3; 5 0 -3; -3 -3 -3")
+
+    kernel2 = np.ones((3,3),np.float32)/25
+
+
+
+    image1 = cv2.filter2D(image,-1,h1);
+    image2 = cv2.filter2D(image,-1,h2);
+    image3 = cv2.filter2D(image,-1,h3);
+    image4 = cv2.filter2D(image,-1,h4);
+    image5 = cv2.filter2D(image,-1,h5);
+    image6 = cv2.filter2D(image,-1,h6);
+    image7 = cv2.filter2D(image,-1,h7);
+    image8 = cv2.filter2D(image,-1,h8);
+    new_image = image1 + image2 + image3 + image4 + image5 +image6 + image7 + image8
+    #show_image(new_image,"Kirsch")
+
+    gray = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
+    #show_image(gray,"gray Kirsch")
+    #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(2,2))
+    #gray = clahe.apply(gray)
+    #show_image(new_image,"clahe Kirsch")
+    veins = threshold(gray,77)
+
+    show_image(veins," veins Kirsch")
+
+    #new_image = cv2.GaussianBlur(veins,(3,3),0)
+    #show_image(new_image,"erode")
+
+    return veins
+
+def detect_optical_disc(image):
+    #original_image = copy.copy(image)
     #show_image(image,"normal")
 
     average = np.average(image)
@@ -133,7 +291,8 @@ def detect_optical_disc(image):
 
 
     b,g,r = cv2.split(image)
-
+    show_image(g,"verde")
+    show_image(r,"red")
     # create a CLAHE object (Arguments are optional).
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(2,2))
     new_image = clahe.apply(g)
@@ -150,24 +309,28 @@ def detect_optical_disc(image):
     #dark_pixel = removing_dark_pixel(new_image)
     #show_image(new_image,"removing removing_dark_pixel")
     #RRO = RRO_thresholding.RRO_thresholding()
-    #new_image = RRO.calculate(new_image)
+    #new_image = RRO.calculate(dark_pixel)
 
-    #new_image = threshold(new_image,90)
+    #new_image = threshold(g,average)
 
-    #new_image = cv2.Canny(new_image,80,80)
+    #new_image = cv2.Canny(g,100,100)
+    #ret,new_image = cv2.threshold(g, 0, 255, cv2.THRESH_OTSU)
     new_image = sobel(new_image)
 
     #new_image = cv2.adaptiveThreshold(new_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-    show_image(new_image,"bordes")
+
+    #show_image(new_image,"bordes")
 
 
-
+    #new_image = cv2.GaussianBlur(new_image,(3,3),0)
+    #show_image(new_image,"x")
     new_image = cv2.dilate(new_image, None, iterations=1)
-    #show_image(new_image,"dilate")
+    show_image(new_image,"dilate")
     #new_image = cv2.erode(new_image, None, iterations=1)
     #show_image(new_image,"erode")
+    veins = detect_veins(image)
 
-    detecte_circles(new_image,g)
+    detect_circles(new_image,veins,g)
 
 
 
