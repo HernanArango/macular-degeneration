cb65307f0e811a34667c74eac7a7f76c56b4797a
Hernan <hernantion1@yahoo.com>
2020-01-30 19:36:38 -0500

    deleting unnecessary files

--- dmae.py
+++ dmae.py
@@ -3,11 +3,10 @@ import cv2
 import drusen
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="Mostrar información de depuración", action="store_true")
+parser.add_argument("-v", "--verbose", help="Show proccess information")
 parser.add_argument("-f", "--file", help="Path to image")
 args = parser.parse_args()
- 
-# Aquí procesamos lo que se tiene que hacer con cada argumento
+
 if args.verbose:
     print ("depuración activada!!!")
 if args.file:
